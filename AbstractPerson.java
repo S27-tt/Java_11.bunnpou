7d0aaa8372ad6e540e4753811da5c3d95673b7df
taro <taro@example.com>
2023-05-17 23:47:23 +0900

    firstcommit

---
+++ AbstractPerson.java
@@ -0,0 +1,7 @@
+public abstract class AbstractPerson{
+    abstract protected String getGender();
+
+    public void speak(){
+        System.out.println("I'm a " + getGender());
+    }
+}
---
+++ GenderSpeaker.java
@@ -0,0 +1,9 @@
+public class GenderSpeaker{
+    public static void main(String[] args){
+        doSpeak(new Man());
+        doSpeak(new Woman());
+    }
+    private static void doSpeak(AbstractPerson person){
+        person.speak();
+    }
+}
